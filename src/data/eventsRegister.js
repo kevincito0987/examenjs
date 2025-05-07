0039dad09efbc8a1fe7f0ac52e4c1ec794965240
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-05-07 06:44:19 -0500

    perf: :zap: I've created event schema to save events

---
+++ src/data/eventsRegister.js
@@ -0,0 +1,8 @@
+const registerEvent = () => {
+    let event = {
+        lugar: {required:true, type: String},
+        fechaInicio: {required:true, type: String},
+        fechaFinalizacion: {required:true, type: String},
+        horarios: {required:true, type: String}
+    }
+}
