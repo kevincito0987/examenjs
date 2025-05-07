fef75f42b1ed2de2f77892fef25adcfa8779dca3
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-05-07 07:01:00 -0500

    perf: :zap: I've added some logic to save events.

--- src/data/eventsRegister.js
+++ src/data/eventsRegister.js
@@ -1,11 +1,15 @@
 const registerEvent = () => {
+    let opc = prompt(Number("Ingresa el numnero de eventos que vas a agregar: "))
     let event = {
         lugar: prompt("Indica el lugar del evento: "),
         fechaInicio: prompt("Indica la fecha de inicio del evento: "),
         fechaFinalizacion: prompt("Indica la fecha de finalizacion del evento: "),
         horarios: prompt("Indica los horarios del evento: ")
     }
-
-    let opc = prompt(Number("Ingresa el numnero de eventos que vas a agregar: "))
     let eventosFavoritos = JSON.parse(localStorage.getItem("eventos-favoritos")) || []
+    for (i=0 in opc) {
+        let {...nuevoEvento} = event
+        console.log("Evento guardado: ", eventoGuardado);
+        let eventoGuardado = localStorage.setItem(nuevoEvento)
+    }
 }
